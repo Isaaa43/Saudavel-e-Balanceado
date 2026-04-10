a5c84b6de7f0fb35f80b8bf1a7574b120736bc7d
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-09 23:21:03 -0300

    criado adm basica do jogo
    Criada administração de network, troca de cena

--- luta-magos/scripts/autoloads/troca_cena_temp.gd
+++ luta-magos/scripts/autoloads/troca_cena_temp.gd
@@ -1,12 +1,16 @@
 extends Node
 
 var main_game : Node
+var Network : Node
 
 const MENU_INICIAL = preload("uid://6als3mloubtk")
 const MENU_PARTIDA = preload("uid://c2ppdl2yqxf16")
 
+const GAME_ADM = preload("uid://cm7rbbwccvpap")
+
 func _ready() -> void:
 	main_game = get_node("/root/MainGame")
+	Network = get_node("/root/MainGame/Network")
 
 func go_to_menu_inicial() -> void:
 	for c in main_game.get_children(): c.queue_free()
@@ -17,4 +21,9 @@ func go_to_menu_partida() -> void:
 	for c in main_game.get_children(): c.queue_free()
 	var menu_partida := MENU_PARTIDA.instantiate()
 	main_game.add_child(menu_partida)
+
+func go_to_game() -> void:
+	for c in main_game.get_children(): if c is Control: c.queue_free()
 	
+	var game := GAME_ADM.instantiate()
+	main_game.add_child(game)
