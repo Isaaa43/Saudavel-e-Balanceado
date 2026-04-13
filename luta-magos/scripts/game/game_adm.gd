a279d0d19681995fd1971ffb66d20effd3a77556
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-13 19:24:35 -0300

    Jogador mesh colorido

--- luta-magos/scripts/game/game_adm.gd
+++ luta-magos/scripts/game/game_adm.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("debug"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		NetworkServer.terminar_partida()
