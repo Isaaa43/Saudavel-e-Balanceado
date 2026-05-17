bf5c07d1fe0b3db7477bc3b47afd827d5ba6fbb9
Isaaa43 <isadorafurlanivignatti@gmail.com>
2026-05-16 23:14:43 -0300

    protótipo rodável menu deck build

---
+++ luta-magos/scripts/menus/CardData.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name CardData
+
+@export var nome: String
+@export var custo: int
+@export var tipo: String
+@export_multiline var descricao: String
+@export var icone: Texture2D
