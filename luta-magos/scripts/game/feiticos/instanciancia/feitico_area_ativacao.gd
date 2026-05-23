e608c8e7fe89dc33f0966a3bb3c47a466c4db0fe
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-05-23 19:17:56 -0300

    feat: feitiço atualiza automaticamente camadas fisica3d

--- luta-magos/scripts/game/feiticos/instanciancia/feitico_area_ativacao.gd
+++ luta-magos/scripts/game/feiticos/instanciancia/feitico_area_ativacao.gd
@@ -2,14 +2,21 @@ class_name FeiticoAreaAtivacao
 extends Area3D
 
 func set_feitico_tipo(tipo: Feitico.Tipo) -> void:
+	# Layers: 
+	#	Todos (L 1 = int 1), 
+	#	Todos Feiticos (L 17 = int 65536)
+	collision_layer = (1 + 65536)
+	# ativa tambem a layer do seu tipo especificamente
 	match (tipo):
 		Feitico.Tipo.PROJETIL:
-			pass
+			# Projeteis (L 18)
+			set_collision_layer_value(18, true)
 		Feitico.Tipo.POSICIONADO:
-			pass
+			# Posicionados (L 19)
+			set_collision_layer_value(19, true)
 		Feitico.Tipo.EFEITO:
-			pass
-
+			# Efeitos (L 20)
+			set_collision_layer_value(20, true)
 
 func _ready() -> void:
 	_criar_area()
