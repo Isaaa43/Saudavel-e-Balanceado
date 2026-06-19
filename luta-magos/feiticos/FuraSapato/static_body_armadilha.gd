55c837ed9f17988da50ad74933a56cc202c7a817
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-19 19:44:34 -0300

    feat: armadilha com modelo, animação e desaparece
    Feitiço armadilha foi revigorado com modelo e animações novas
    Armadilha desaparece depois de alguns segundos e reaparece qnd revelada
    Ativação fica aparente enquanto a armadilha recarrega o uso

--- luta-magos/feiticos/FuraSapato/static_body_armadilha.gd
+++ luta-magos/feiticos/FuraSapato/static_body_armadilha.gd
@@ -5,15 +5,25 @@ const REVELDO_MAT = preload("uid://c25lrwh5o1e6r")
 
 @export var mesh_instance_3d: MeshInstance3D
 
+@export var tempo_inicio_ate_invisivel: float = 1.5
+
 var _material_revelado: Material
 
 func _ready() -> void:
 	await get_tree().process_frame
 	toggle_shader_revelacao(false)
 	_material_revelado = REVELDO_MAT
+	# deixa invisivel depois de um tempo inicial
+	get_tree().create_timer(tempo_inicio_ate_invisivel).timeout.connect(deixar_invisivel)
+
+func deixar_invisivel() -> void:
+	mesh_instance_3d.hide()
+func deixar_visivel() -> void:
+	mesh_instance_3d.show()
 
 func toggle_shader_revelacao(ligado: bool) -> void:
 	if ligado:
 		mesh_instance_3d.material_overlay = _material_revelado
+		deixar_visivel()
 	else:
 		mesh_instance_3d.material_overlay = null
