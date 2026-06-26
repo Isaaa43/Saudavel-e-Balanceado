bd4859710edc00930d5d0aa84cf51a20771d3f15
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-26 03:26:05 -0300

    refactor: revelação em área com 2 esferas
    Melhorado o feitiço de revelação em área com 2 esferas.
    Uma com faces para dentro e outra para fora, para ser visível de dentro e fora

--- luta-magos/feiticos/visuais/fv_revelacao.gd
+++ luta-magos/feiticos/visuais/fv_revelacao.gd
@@ -2,12 +2,14 @@ extends Node3D
 class_name FeiticoVisualRevelacao
 
 @export var csg_sphere_3d: CSGSphere3D
+@export var csg_sphere_3d_flip_faces: CSGSphere3D
 @export var gpu_particles_aura: GPUParticles3D
 @export var gpu_particles_particulas: GPUParticles3D
 
 func resize(raio: float) -> void:
 	# mesh
 	csg_sphere_3d.radius = raio
+	csg_sphere_3d_flip_faces.radius = raio
 	# aura
 	var mesh: Mesh = gpu_particles_aura.draw_pass_1
 	mesh.size = Vector2(raio*2, raio*2) # diametro
@@ -17,10 +19,3 @@ func resize(raio: float) -> void:
 	# particulas
 	var proc_mat: ParticleProcessMaterial = gpu_particles_particulas.process_material
 	proc_mat.emission_sphere_radius = raio
-
-# TODO: ver isso, (aparecer o circulo para quem esta dentro
-var rodar: bool = false
-func _process(delta: float) -> void:
-	if rodar:
-		csg_sphere_3d.flip_faces = !csg_sphere_3d.flip_faces
-	rodar = not rodar
