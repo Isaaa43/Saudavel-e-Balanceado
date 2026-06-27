10190a305f418e43be69543db911edf9e6f666b6
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-27 20:40:42 -0300

    perf(lobby): removido seleção de feitiços de teste
    Removido código e UI referente a primeira seleção de feitiços, criado para testes

--- luta-magos/scripts/menus/menu_partida.gd
+++ luta-magos/scripts/menus/menu_partida.gd
@@ -1,24 +1,13 @@
 extends Control
 class_name MenuPartida
 
-# TODO: remover isso
-@export var debug_ignorar_checks := true
-
-@export var qntd_magias_grimorio : int = 4
-
-@onready var grid_container: GridContainer = $VBoxMagias/GridContainer
-@onready var h_box_container: HBoxContainer = $VBoxMagias/HBoxContainer
 @onready var button_comecar: Button = $VBoxPartida/ButtonComecar
 @onready var button_sair: Button = $VBoxPartida/ButtonSair
 
-@onready var label_log: Label = $VBoxContainer/LabelLog
+@onready var label_log: Label = $VBoxLogs/LabelLog
 
 @onready var grid_deck: GridContainer = $Deck/ScrollContainer/GridDeck
 @onready var img_card_ref: TextureRect = $Deck/ScrollContainer/ImgCardRef
-
-
-var buttons_selecionados_list : Array[Button] = []
-var passiva_selecionada : Button = null
 
 # TODO: solucao melhor
 # TODO: Criar autoload para manter o log das acoes de rede
@@ -33,15 +22,6 @@ func _ready() -> void:
 	Network.logs.update_conexao_texto.connect(add_log)
 	
 	_mostrar_deck()
-	
-	# pego os botoes da grid
-	for button : Button in grid_container.get_children().filter(func(a): return a is Button):
-		button.pressed.connect(clicado.bind(button))
-	#
-	for button : Button in h_box_container.get_children().filter(func(a): return a is Button):
-		button.pressed.connect(passiva_select.bind(button))
-	# 
-	verificar_partida_comecar()
 
 
 func _mostrar_deck() -> void:
@@ -54,47 +34,19 @@ func _mostrar_deck() -> void:
 		img.texture = card.icone
 		grid_deck.add_child(img)
 		img.show()
+	
+	# atualiza o botao comecar
+	atualizar_botao_comecar_partida()
 
-func verificar_partida_comecar() -> void:
+func atualizar_botao_comecar_partida() -> void:
+	
+	var valido: bool = GlobalDeck.deck_size == GlobalDeck.get_deck().size()
+	button_comecar.disabled = not valido
+	
 	# TODO: Por enquato vai isso, mas no futuro fazer um sistema de voto, tipo ready DBD
 	# somente o server pode iniciar a partida
-	button_comecar.disabled = not multiplayer.is_server()
+	#button_comecar.disabled = not multiplayer.is_server()
 	return
-	
-	# TODO: remover
-	if debug_ignorar_checks: 
-		button_comecar.disabled = false 
-		return
-	
-	var condicao_comecar : bool = buttons_selecionados_list.size() == qntd_magias_grimorio
-	condicao_comecar = condicao_comecar and (passiva_selecionada != null)
-	button_comecar.disabled = not condicao_comecar
-
-func clicado(button : Button):
-	# ja tem -> tira da lista
-	if buttons_selecionados_list.has(button):
-		button.flat = false
-		buttons_selecionados_list.erase(button)
-		verificar_partida_comecar()
-		return
-	# add na lista
-	buttons_selecionados_list.append(button)
-	button.flat = true
-	# se tiver mts na lista -> tira o ultimo
-	if buttons_selecionados_list.size() > qntd_magias_grimorio:
-		clicado(buttons_selecionados_list[0])
-	
-	verificar_partida_comecar()
-
-func passiva_select(button : Button) -> void:
-	# desativa o anterior, se tiver
-	if passiva_selecionada != null:
-		passiva_selecionada.flat = false
-	# seleciona o atual
-	passiva_selecionada = button
-	passiva_selecionada.flat = true
-	
-	verificar_partida_comecar()
 
 func _on_button_comecar_pressed() -> void:
 	if not is_multiplayer_authority(): return
