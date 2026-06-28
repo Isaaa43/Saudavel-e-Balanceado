446644a616e40e8e093299afdaa7a48f176dccdf
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-28 01:42:46 -0300

    feat: adicionado verificação para começar o treino
    Corrigido typo no texto da label de verificar o grimorio

--- luta-magos/scripts/menus/menu_treino.gd
+++ luta-magos/scripts/menus/menu_treino.gd
@@ -1,10 +1,21 @@
 class_name MenuTreino
 extends Control
 
-@onready var button_comecar: Button = $VBoxPartida/ButtonComecar
+@onready var button_comecar: Button = $%ButtonComecar
+@onready var label_ajuste_grimorio: Label = %LabelAjusteGrimorio
+@onready var display_grimorio: DisplayGrimorio = $DisplayGrimorio
 
 func _ready() -> void:
 	button_comecar.grab_focus()
+	label_ajuste_grimorio.hide()
+	_verificar_comecar()
+	display_grimorio.grimorio_atualizado.connect(_verificar_comecar)
+
+func _verificar_comecar() -> void:
+	var invalido: bool = GlobalDeck.deck_size != GlobalDeck.get_deck().size()
+	# libera o botao somente se valido
+	button_comecar.disabled = invalido
+	label_ajuste_grimorio.visible = invalido
 
 func _on_button_comecar_pressed() -> void:
 	TrocaCenaTemp.go_to_treino()
