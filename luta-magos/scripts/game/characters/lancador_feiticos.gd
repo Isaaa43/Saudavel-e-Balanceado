3398e22fa77082ca7962cadf0cdc7ecbd7eaa2fd
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-29 02:04:59 -0300

    refactor: reorganizado codigo lancado de feiticos
    Trocado nome de funções
    Organizada as posicoes das funções no código por sentido

--- luta-magos/scripts/game/characters/lancador_feiticos.gd
+++ luta-magos/scripts/game/characters/lancador_feiticos.gd
@@ -22,49 +22,20 @@ var selecao_feitico_id: int = 0
 
 func _ready() -> void:
 	audio_stream_player.stream = audio_cast
-	print(GlobalDeck.feiticos_id_escolhidos)
+
+# Capturar Inputs
+# -----------------------------------------------------------------------------
 
 func _input(_event: InputEvent) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE: return
 	
 	if Input.is_action_just_pressed("acao"):
-		_escolher_feitico()
-
-func _mostrar_custo_mana(feitico_id: String) -> void:
-	var feitico_def : FeiticoDef = registro_feiticos.get_feitico(feitico_id)
-	var custo_mana : float = feitico_def.custo
-	if sistema_mana.tem_mana_suficiente(custo_mana):
-		var custo_porcent : float = sistema_mana.calc_porcentegem_mana(custo_mana)
-		hud_jogador.atualizar_custo_mana_previsto(custo_porcent)
-	else:
-		hud_jogador.atualizar_custo_mana_previsto(0.0)
-
-func _get_feitico_id_from_id(id: int) -> String:
-	match id:
-		0:
-			return "BolaFogo"
-		1:
-			return "FuraSapato"
-		2:
-			return "PuloImpulsionado"
-		3:
-			return "Ozempagic"
-		4:
-			return "ToTeVendo"
-		5:
-			return "Geladinho"
-	return ""
-
-func _escolher_feitico() -> void:
-	
-	var feitico_id : String = hud_jogador.get_feitico_id_from_idx()
-	if feitico_id != "":
-		lancar_feitico_escolhido(feitico_id)
+		# apertar
+		_tentar_lancar_feitico()
 
 func _process(delta: float) -> void:
 	for id in _cooldowns:
 		_cooldowns[id] = maxf(0.0, _cooldowns[id] - delta)
-	
 	
 	if Input.is_action_just_pressed("feitico_prox"):
 		hud_jogador.add_idx(1)
@@ -73,28 +44,22 @@ func _process(delta: float) -> void:
 		hud_jogador.add_idx(-1)
 		_mostrar_custo_mana(hud_jogador.get_feitico_id_from_idx())
 
-## Retorna a direcao normalizada que o lancador de feiticos esta mirando
-func get_direcao_mirando() -> Vector3:
-	var direcao : Vector3 = -get_global_transform().basis.z
-	return direcao.normalized()
 
-## Retorna a posicao global do lancador de feiticos ao fazer um raycast.
-## 		Retorna Vector3.INF se o raycast nao colidir
-func get_posicao_global_mirando() -> Vector3:
-	# TODO: corrigir isso, problema que fazer o await cria uma cadeia de await
-	# 		criar func especifica para posicionar (feiticos que usam raycast)
-	## so executa o raycast dentro do physics_frame
-	#if not Engine.is_in_physics_frame():
-		#await get_tree().physics_frame
-	#
-	ray_cast_visao.force_raycast_update()
-	if ray_cast_visao.is_colliding():
-		var posicao_global := ray_cast_visao.get_collision_point()
-		return posicao_global
-	# 
-	return Vector3.INF
+# Lancar Feitico
+# -----------------------------------------------------------------------------
 
-func lancar_feitico_escolhido(feitico_id: String) -> void:
+func _tentar_lancar_feitico() -> void:
+	var feitico_id : String = get_feitico_escolhido()
+	if feitico_id == "": return
+	
+	processar_lancar_feitico(feitico_id)
+
+## Retorna o feitico que a hud tem como escolhido atualmente
+func get_feitico_escolhido() -> String:
+	return hud_jogador.get_feitico_id_from_idx()
+
+## Processa cooldown, custo de mana, contexto para lancar o feitico 
+func processar_lancar_feitico(feitico_id: String) -> void:
 	# --- Faz as verificacoes antes de lancar ---
 	# se estiver no cooldown, nao continue
 	if _cooldowns.get(feitico_id, 0) > 0.1: return
@@ -123,8 +88,44 @@ func _criar_feitico_contexto(feitico_def : FeiticoDef) -> FeiticoContexto:
 	var feitico_contexto := FeiticoContexto.criar(feitico_def, self)
 	return feitico_contexto
 
-# emite que esta lancando um feitico
+## emite que esta lancando um feitico
 func _lancar_feitico(feitico_contexto : FeiticoContexto) -> void:
 	lancar_feitico.emit(feitico_contexto)
 	# emite o audio
 	audio_stream_player.play()
+
+# Diracao da Mira
+# -----------------------------------------------------------------------------
+
+## Retorna a direcao normalizada que o lancador de feiticos esta mirando
+func get_direcao_mirando() -> Vector3:
+	var direcao : Vector3 = -get_global_transform().basis.z
+	return direcao.normalized()
+
+## Retorna a posicao global do lancador de feiticos ao fazer um raycast.
+## 		Retorna Vector3.INF se o raycast nao colidir
+func get_posicao_global_mirando() -> Vector3:
+	# TODO: corrigir isso, problema que fazer o await cria uma cadeia de await
+	# 		criar func especifica para posicionar (feiticos que usam raycast)
+	## so executa o raycast dentro do physics_frame
+	#if not Engine.is_in_physics_frame():
+		#await get_tree().physics_frame
+	#
+	ray_cast_visao.force_raycast_update()
+	if ray_cast_visao.is_colliding():
+		var posicao_global := ray_cast_visao.get_collision_point()
+		return posicao_global
+	# 
+	return Vector3.INF
+
+# Mostrar Mana
+# -----------------------------------------------------------------------------
+
+func _mostrar_custo_mana(feitico_id: String) -> void:
+	var feitico_def : FeiticoDef = registro_feiticos.get_feitico(feitico_id)
+	var custo_mana : float = feitico_def.custo
+	if sistema_mana.tem_mana_suficiente(custo_mana):
+		var custo_porcent : float = sistema_mana.calc_porcentegem_mana(custo_mana)
+		hud_jogador.atualizar_custo_mana_previsto(custo_porcent)
+	else:
+		hud_jogador.atualizar_custo_mana_previsto(0.0)
