77f3168cb3bcf1169cbe0fbd2677b9b28774cf07
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-29 00:48:08 -0300

    feat: Explosão da bomba criada
    Criado comportamento para explosão da bomba
    Alterado bola de fogo para ter resize e esfera visivel de dentro

--- luta-magos/feiticos/visuais/fv_bola_fogo.gd
+++ luta-magos/feiticos/visuais/fv_bola_fogo.gd
@@ -1,8 +1,20 @@
+class_name FeiticoVisualBolaFogo
 extends Node3D
 
-@onready var fogo: GPUParticles3D = $CSGSphere3D/fogo
+@export var fogo: GPUParticles3D
+@export var csg_sphere_3d: CSGSphere3D
+@export var csg_sphere_3d_flip: CSGSphere3D
 
 func _ready() -> void:
 	fogo.hide()
 	await get_tree().create_timer(0.3).timeout
 	fogo.show()
+	csg_sphere_3d_flip.hide()
+
+func resize(raio: float) -> void:
+	# mesh
+	csg_sphere_3d.radius = raio
+	csg_sphere_3d_flip.radius = raio
+	# trail
+	#var mesh: Mesh = fogo.draw_pass_1
+	#mesh.size = Vector2(raio*2, raio*2) # diametro
--- luta-magos/scripts/game/feiticos/definicoes/comportamento_def/FC_bomba_def.gd
+++ luta-magos/scripts/game/feiticos/definicoes/comportamento_def/FC_bomba_def.gd
@@ -8,10 +8,15 @@ extends FeiticoComportamentoDef
 
 @export_flags_3d_physics var mascara_impacto = 1
 
+@export var explosao_comportamento_def: FeiticoComportamentoDef
+
 func _criar() -> FeiticoComportamentoBomba:
 	var comportamento := FeiticoComportamentoBomba.new(duracao_seg)
 	
 	comportamento.velocidade = velocidade
 	comportamento.tamanho_raio = tamanho_raio
+	
+	if explosao_comportamento_def:
+		comportamento.explosao_comportamento_def = explosao_comportamento_def
 	
 	return comportamento
