402d42a2f870cb76ace701af6b82dbc6727e09c9
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-29 00:57:35 -0300

    fix: explosa nao mostra mais o trail

--- luta-magos/feiticos/visuais/fv_bola_fogo.gd
+++ luta-magos/feiticos/visuais/fv_bola_fogo.gd
@@ -5,13 +5,21 @@ extends Node3D
 @export var csg_sphere_3d: CSGSphere3D
 @export var csg_sphere_3d_flip: CSGSphere3D
 
+var resized : bool = false
+
 func _ready() -> void:
 	fogo.hide()
 	await get_tree().create_timer(0.3).timeout
-	fogo.show()
-	csg_sphere_3d_flip.hide()
+	# TODO: melhorar
+	if resized:
+		csg_sphere_3d_flip.hide()
+	else:
+		# se nao foi alterado o tamanho, (ou seja eh a bola de fogo normal)
+		# mostra o trail novamente
+		fogo.show()
 
 func resize(raio: float) -> void:
+	resized = (raio >= 1)
 	# mesh
 	csg_sphere_3d.radius = raio
 	csg_sphere_3d_flip.radius = raio
