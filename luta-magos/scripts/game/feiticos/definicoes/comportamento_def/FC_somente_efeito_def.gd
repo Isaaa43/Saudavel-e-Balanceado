78502c5c82a0d04ab0e0156ec79a2c3840c09479
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-29 05:23:45 -0300

    feat(somente efeito): duração na aplicação do efeito
    Comportamento de somente efeito com duração na existência da área aplicação do efeito
    Aplicado ao pulo

--- luta-magos/scripts/game/feiticos/definicoes/comportamento_def/FC_somente_efeito_def.gd
+++ luta-magos/scripts/game/feiticos/definicoes/comportamento_def/FC_somente_efeito_def.gd
@@ -3,7 +3,11 @@ extends FeiticoComportamentoDef
 
 @export_group("Somente Efeito")
 
+## Tempo em que a area de efeito existe (para aplicar efeito nos corpos que entrarem na area)
+@export var duracao_aplicacao_efeito_seg : float = 0.1
+
 func _criar() -> FeiticoComportamentoSomenteEfeito:
 	var comportamento := FeiticoComportamentoSomenteEfeito.new(duracao_seg)
+	comportamento.duracao_aplicacao_efeito_seg = duracao_aplicacao_efeito_seg
 	
 	return comportamento
