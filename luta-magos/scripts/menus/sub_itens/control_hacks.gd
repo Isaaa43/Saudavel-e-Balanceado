ee91c5ad6387de548ef913e4491439685869a710
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-07-08 00:53:07 -0300

    feat: botão de reset nos hacks treino

--- luta-magos/scripts/menus/sub_itens/control_hacks.gd
+++ luta-magos/scripts/menus/sub_itens/control_hacks.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var spin_box_cartas_deck: SpinBox = $GridContainer/SpinBoxCartasDeck
 
 func _ready() -> void:
+	_display_valores()
+
+func _display_valores() -> void:
 	spin_box_mana.set_value_no_signal(GlobalDeck.treino_mana_regen)
 	spin_box_cartas_deck.set_value_no_signal(GlobalDeck.treino_deck_size)
 
@@ -12,3 +15,9 @@ func _on_spin_box_mana_value_changed(value: float) -> void:
 
 func _on_spin_box_cartas_deck_value_changed(value: float) -> void:
 	GlobalDeck.treino_deck_size = int(value)
+
+
+func _on_button_reset_pressed() -> void:
+	GlobalDeck.treino_mana_regen = GlobalDeck.mana_regen
+	GlobalDeck.treino_deck_size = GlobalDeck.deck_size
+	_display_valores()
