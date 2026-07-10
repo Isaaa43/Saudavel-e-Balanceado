f8b822a5b8cab3002adb77c0b2a37a374a715a58
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-07-09 23:08:48 -0300

    refactor: tamanho organizado no help
    tamanho do ícone limitado no menu help, para manter o mesmo padrão em descrições de múltiplas linhas

--- luta-magos/scripts/game/UI/hud_help_feitico.gd
+++ luta-magos/scripts/game/UI/hud_help_feitico.gd
@@ -9,3 +9,5 @@ func set_feitico(feitico_card: MenuDeck.CardData) -> void:
 	texture_rect.texture = feitico_card.icone
 	label_nome.text = feitico_card.nome
 	label_descricao.text = feitico_card.descricao
+	# remove quebra de linhas
+	label_descricao.text = label_descricao.text.replace("\n", " ")
